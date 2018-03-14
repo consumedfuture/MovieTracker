86d3d4b9f94fe65b15ba20e96d60b43f05cd1ebd
Alexander Lychagov <lychagovan@miac74.ru>
2018-03-14 16:18:02 +0500

    search for films in db

--- app/src/main/java/lychagov/com/movietracker/MainActivity.kt
+++ app/src/main/java/lychagov/com/movietracker/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
 import com.google.gson.Gson
@@ -13,12 +12,10 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jetbrains.anko.*
 import org.jetbrains.anko.custom.customView
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.frameLayout
-import org.jetbrains.anko.padding
-import org.jetbrains.anko.textColor
-import org.w3c.dom.Text
+import org.jetbrains.anko.sdk25.coroutines.textChangedListener
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,15 +28,36 @@ class MainActivity : AppCompatActivity() {
         val filmsAdapter = FilmsListAdapter(filmsList)
 
         frameLayout {
-            customView<RecyclerView> {
-                layoutManager = LinearLayoutManager(context)
-                adapter = filmsAdapter
-            }
-        }
-        loadFilms(httpClient) {films ->
-            runOnUiThread {
-                filmsList.addAll(films)
-                filmsAdapter.notifyDataSetChanged()
+                val editText = editText {
+                    hint = "films, series, ..."
+                }.lparams(width = matchParent) {
+                    height = dip(50)
+                    padding = dip(10)
+
+                }
+                customView<RecyclerView> {
+                    layoutManager = LinearLayoutManager(context)
+                    adapter = filmsAdapter
+                }.lparams{
+                    topMargin = dip(50)
+                }
+            editText.textChangedListener {
+                afterTextChanged {
+                    //Log.v("films", it?.toString())
+                    if (it?.isBlank() == false)
+                        loadFilms(httpClient, editText.text.toString()) { films ->
+                            runOnUiThread {
+                                filmsList.clear()
+                                filmsList.addAll(films)
+                                filmsAdapter.notifyDataSetChanged()
+                            }
+                        }
+                    else{
+                        filmsList.clear()
+                        filmsAdapter.notifyDataSetChanged()
+                    }
+
+                }
             }
         }
     }
@@ -53,17 +71,17 @@ class TMDBFilms : ArrayList<TMDBFilmInfo>()
 
 fun loadFilms(
         httpClient: OkHttpClient,
+        title: String,
         onComplete: (films: TMDBFilms) -> Unit
 ){
     Thread {
         val request = Request.Builder()
-                .url("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b7c5b3c41053880dab8271e0fa4864da&language=ru")
+                .url("https://api.themoviedb.org/3/search/movie?query=${URLEncoder.encode(title,"utf-8")}&api_key=b7c5b3c41053880dab8271e0fa4864da&language=ru")
                 .build()
         val response = httpClient.newCall(request).execute()
         val obj = JsonParser().parse(response.body()?.string())
         val text = obj.asJsonObject.get("results") ?: JsonObject()
         val films: TMDBFilms = Gson().fromJson(text,TMDBFilms::class.java)
-
         onComplete(films)
     }.start()
 }
@@ -77,15 +95,19 @@ class FilmsListAdapter(
         val textView = TextView(parent!!.context).apply {
             textSize = 32f
             textColor = Color.BLUE
-            padding = dip(16)
+            padding = dip(10)
+            isClickable = true
+
         }
+
         return FilmViewHolder(textView)
     }
 
     override fun onBindViewHolder(holder: FilmViewHolder?, position: Int) {
-        if (holder==null) return
+        if (holder == null) return
         holder.filmName.text = films[position].title
     }
+
     class FilmViewHolder(
             val filmName: TextView
     ) : RecyclerView.ViewHolder(filmName)
